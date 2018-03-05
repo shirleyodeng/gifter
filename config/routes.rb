15a41d1eef3044e5d1a0df2f3d96fd8d84430b77
Shirley Deng <shirleeey@live.com>
2018-03-05 15:10:40 +0000

    updated forms

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :events, except: [:edit, :update] do
+  resources :events, except: [:show, :edit, :update] do
     resources :gifts
   end
 

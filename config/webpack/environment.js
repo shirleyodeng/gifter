b28c43198b4d8d20fe349fa338bcb5150e88530b
Kateryna Merkalenko <kateryna.m@hotmail.com>
2018-03-07 18:34:29 +0000

    made fixes for heroku and webpacker by reading arthurs instructions

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,15 @@
+// const { environment } = require('@rails/webpacker')
+
+// module.exports = environment
 const { environment } = require('@rails/webpacker')
+
+// Bootstrap 3 has a dependency over jQuery:
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery'
+  })
+)
 
 module.exports = environment

aaedf50b48b7682e9821f43fe042a9ba06728ed4
Shirley Deng <shirleeey@live.com>
2018-03-07 16:30:59 +0000

    dashboard fixed

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User", foreign_key: "user_id"
   has_many :gifts, dependent: :destroy
-  has_many :guests
+  has_many :guests, dependent: :destroy
   has_many :users, through: :guests
   has_many :invites
   validates :name, presence: true

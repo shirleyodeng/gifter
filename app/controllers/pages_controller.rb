5650780a3eb4fcd90f58a4d1f2f7ddc5f6fbed7c
Shirley Deng <shirleeey@live.com>
2018-03-08 21:29:26 +0000

    footer basic done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-  end
-
-  def errors
+    @invite = Invite.new #tempcode
   end
 end

bd2aa609b900c53e1c61c228149e8bc21a479ac6
Shirley Deng <shirleeey@live.com>
2018-03-15 11:36:59 +0000

    sessions updated

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,8 +1,9 @@
 class Users::SessionsController < Devise::SessionsController
   def after_sign_in_path_for(resource)
-    if params[:event_id] != ''
+    if params[:event_id].nil? || params[:event_id] = ''
+      return events_path
+    else
       return event_gifts_path(params[:event_id])
     end
-    super
   end
 end

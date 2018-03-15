068cbda3b07403b68b61da5708ff3ef4b5525aa4
Shirley Deng <shirleeey@live.com>
2018-03-15 22:16:39 +0000

    login fixes

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,4 +1,10 @@
 class Users::SessionsController < Devise::SessionsController
+  # def create
+  #   #@event_id = params[:event_id]
+  #   super
+  #   session[:event_id] = resource.event_id
+  # end
+
   def after_sign_in_path_for(resource)
     if params[:invite_token]
       event = Invite.find_by_token(params[:invite_token]).event
@@ -6,6 +12,8 @@ class Users::SessionsController < Devise::SessionsController
     elsif params[:event_id].nil? || params[:event_id] == ''
       return events_path
     else
+      event = Event.find(params[:event_id])
+      current_user.events.push(event)
       return event_gifts_path(params[:event_id])
     end
   end

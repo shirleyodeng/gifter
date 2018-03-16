e09f245985889f70b83f4ba61501dd07dc5cb0bb
Shirley Deng <shirleeey@live.com>
2018-03-16 11:56:26 +0000

    convos updated

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -3,8 +3,8 @@ class Conversation < ActiveRecord::Base
   belongs_to :recipient, class_name: 'User', foreign_key: :recipient_id
   belongs_to :event
   has_many :messages, dependent: :destroy
-  validates_uniqueness_of :sender_id, scope: :recipient_id
-  scope :between, -> (sender_id,recipient_id) do
+  validates :sender_id, :uniqueness => {:scope => [:recipient_id, :event_id]}
+  scope :between, -> (sender_id, recipient_id) do
     where("(conversations.sender_id = ? AND conversations.recipient_id = ? ) OR (conversations.sender_id = ? AND conversations.recipient_id = ? )", sender_id, recipient_id, recipient_id, sender_id)
   end
 end
